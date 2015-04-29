627bdb1eb539736e6e5f10288b884235985f5588
Noel Bacani <mail@ndovutech.com>
2015-04-29 09:30:46 -0700

    added some styling

--- app/assets/javascripts/components/home_screen.js.jsx
+++ app/assets/javascripts/components/home_screen.js.jsx
@@ -3,8 +3,8 @@ var HomeScreen = React.createClass({
   render: function() {
     return (
       <div>
-        <h1>Hello!</h1>
-        <p>This is some text from react!</p>
+        <h1 className="title">Hello!</h1>
+        <p className="content">This is some text from react!</p>
       </div>
     )
   }
