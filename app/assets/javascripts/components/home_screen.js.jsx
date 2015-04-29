1fba51e491f4646058492995b2362fd124ac4d55
Noel Bacani <mail@ndovutech.com>
2015-04-29 09:36:52 -0700

    look an array

--- app/assets/javascripts/components/home_screen.js.jsx
+++ app/assets/javascripts/components/home_screen.js.jsx
@@ -7,6 +7,8 @@ var HomeScreen = React.createClass({
         <h1 className="title">Hello!</h1>
         <p className="content">
           This is some text from react!
+          { /* this is a comment */}
+          { [1,2,3,4,5].join(' ') }
           {1 + 1}
           {name}
         </p>
