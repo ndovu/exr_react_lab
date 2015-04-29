2a40317bfd856a2b6a6bc36f991a6db553d1ebb9
Noel Bacani <mail@ndovutech.com>
2015-04-29 09:32:55 -0700

    look some js is added. cool

--- app/assets/javascripts/components/home_screen.js.jsx
+++ app/assets/javascripts/components/home_screen.js.jsx
@@ -1,12 +1,14 @@
 var HomeScreen = React.createClass({
 
   render: function() {
+    var name = 'Noel';
     return (
       <div>
         <h1 className="title">Hello!</h1>
         <p className="content">
           This is some text from react!
           {1 + 1}
+          {name}
         </p>
       </div>
     )
