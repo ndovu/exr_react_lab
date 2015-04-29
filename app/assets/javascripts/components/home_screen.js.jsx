62b46da092228f9b4a2c9a2653e78533a89322e0
Noel Bacani <mail@ndovutech.com>
2015-04-29 14:08:30 -0700

    more practice using react components by creating a form, submit, and update the view w00t

--- app/assets/javascripts/components/home_screen.js.jsx
+++ app/assets/javascripts/components/home_screen.js.jsx
@@ -1,136 +1,37 @@
-function reverse(s) {
-  var o = '';
-  for (var i = s.length - 1; i >= 0; i--)
-    o += s[i];
-  return o;
-}
-
-var Greeting = React.createClass({
-  getDefaultProps: function() {
-    return { name: 'stranger' };
-  },
-
+var HomeScreen = React.createClass({
   getInitialState: function() {
-    return { language: 'english' }
+    return {
+      nameSubmitted: false,
+      name: ''
+    }
   },
 
-  useSpanish: function() {
-    this.setState({language: 'spanish'});
+  submitName: function(e) {
+    e.preventDefault();
+    this.setState({nameSubmitted: true});
   },
 
-  useEnglish: function() {
-    this.setState({language: 'english'});
+  updateName: function() {
+    this.setState({name: this.refs.nameInput.getDOMNode().value});
   },
 
   render: function() {
-    if (this.state.language == 'english') {
+    if (this.state.nameSubmitted) {
       return (
-        <div className="greeting">
-          <h1 className="title">
-            Hello {reverse(this.props.name)}! <a onClick={this.useSpanish}>(es)</a>
-          </h1>
-          {this.props.children}
+        <div>
+          <h1>Hello {this.state.name}!</h1>
+          <button onClick={this.reset}>OK!</button>
         </div>
       )
     } else {
       return (
-        <div className="greeting">
-          <h1 className="title">
-            Hola {reverse(this.props.name)}ito! <a onClick={this.useEnglish}>(en)</a>
-          </h1>
-          {this.props.name}
-        </div>
+        <form onSubmit={this.submitName}>
+          <input type="text" ref="nameInput"
+                 placeholder="What is your name?"
+                 onChange={this.updateName} />
+          <input type="submit" />
+        </form>
       )
     }
   },
-
-});
-
-var ClickCounter = React.createClass({
-  getDefaultProps: function() {
-    return { initialValue: 0, title: 'Click counter' }
-  },
-
-  getInitialState: function() {
-    return { count: this.props.initialValue };
-  },
-
-  incrementClickCount: function() {
-    this.props.onIncrement();
-    this.setState({count: this.state.count + 1});
-  },
-
-  render: function() {
-    return (
-      <p onClick={this.incrementClickCount}>
-        {this.props.title}: {this.state.count}
-      </p>
-    );
-  },
-});
-
-var HomeScreen = React.createClass({
-  getInitialState: function() {
-    return { sum: 0, items: [] };
-  },
-
-  increaseSum: function () {
-    this.setState({sum: this.state.sum + 1});
-  },
-
-  updateText: function() {
-    this.setState({text: this.refs.textInput.getDOMNode().value});
-  },
-
-  addItem: function(e) {
-    // Prevent default form action
-    e.preventDefault();
-    // Set the state to append the text to some array
-    var items = this.state.items;
-    items.push(this.state.text);
-    this.setState({items: items, text: ''});
-  },
-
-  render: function() {
-    return (
-      <div>
-        <Greeting name="Brent">
-          <p>This is from inside of the greeting!</p>
-          <p>More stuff!</p>
-          <p>More stuff!</p>
-        </Greeting>
-
-        <form onSubmit={this.addItem}>
-          <input  type="text" 
-                  ref="textInput" 
-                  value={this.state.text}
-                  onChange={this.updateText} />
-          <button>Submit</button>
-        </form>
-        
-        <ul>
-          {
-            this.state.items.map(function(item) {
-              return <li>{item}</li>
-            })
-          }
-        </ul>
-
-        <p className="content">
-          This is some text coming from a React component!
-        </p>
-
-        <ClickCounter onIncrement={this.increaseSum}
-                      initialValue={0} 
-                      title="Click counter one" />
-
-        <ClickCounter onIncrement={this.increaseSum}
-                      initialValue={0} 
-                      title="Click counter two" />
-
-        <ClickCounter />
-      </div>
-    );
-  }
-
 });
